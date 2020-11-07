44b3aa52dc56cdd9ea24ab8c323628d08d02bcff
mohamedfab <mo7amed.fawzy33@gmail.com>
2020-11-07 11:27:16 +0200

    Adding Header Guard For Keypad and Timer drivers

---
+++ HAL/KeyPad/Inc/keypad.h
@@ -0,0 +1,12 @@
+/***************************************
+ * keypad.h
+ *  Created on: Oct 24, 2020
+ *  Author: Eng_Fawzi
+ ***************************************/
+#ifndef HAL_KEYPAD_INC_KEYPAD_H_
+#ifndef HAL_KEYPAD_INC_KEYPAD_H_
+
+
+
+
+#endif
---
+++ MCAL/Timer/Inc/timer.h
@@ -0,0 +1,13 @@
+/*
+ * timer.h
+ *  Created on: Oct 24, 2020
+ *  Author: Eng_Fawzi
+ */
+
+
+#ifndef MCAL_TIMER_INC_TIMER_H_
+#ifndef MCAL_TIMER_INC_TIMER_H_
+
+
+
+#endif
